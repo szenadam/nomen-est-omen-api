afb1623819c0a9743d6d2795af63f482bca54193
szenadam <szenadam1@gmail.com>
2022-12-20 13:14:38 +0100

    Add names database in json, create db interfaces

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,29 @@ import * as morgan from 'morgan';
 const app: Application = express();
 const port: number = 3000;
 
-interface IRomanName {
-  praenomen: string;
+const db: IRomanNameDb = require('../data/roman-names.json');
+const {praenomines, nomines, cognomines } = db;
+
+interface IPraenomen {
+  name: string;
+  abbr: string;
+  source: string;
+}
+
+interface INomen {
   nomen: string;
-  cognomen: string;
+  source: string;
+}
+
+interface ICognomen {
+  name: string;
+  source: string;
+}
+
+interface IRomanNameDb {
+  praenomines: IPraenomen[];
+  nomines: INomen[];
+  cognomines: ICognomen[];
 }
 
 app.use(morgan('combined'));
@@ -18,12 +37,10 @@ app.get("/", (_: Request, res: Response) => {
 });
 
 app.get("/name", (_: Request, res: Response) => {
-  const name: IRomanName = {
-    praenomen: "Titus",
-    nomen: "Flavius",
-    cognomen: "Petro"
-  }
-  res.json({ name: `${name.praenomen} ${name.nomen} ${name.cognomen}` });
+  const praenomen = praenomines[Math.floor(Math.random() * praenomines.length)]
+  const nomen = nomines[Math.floor(Math.random() * nomines.length)]
+  const cognomen = cognomines[Math.floor(Math.random() * cognomines.length)]
+  res.json({ name: `${praenomen.name} ${nomen.nomen} ${cognomen.name}` });
 });
 
 app.listen(port, () => console.log(`API is listening on port ${port}!`));
