d4b54954cacbe959be4c71f5755180e77b96938a
szenadam <szenadam1@gmail.com>
2022-12-20 12:21:30 +0100

    Add a sample endpoint for roman names

--- src/start.ts
+++ src/start.ts
@@ -1,9 +1,26 @@
 import express = require("express");
-import { Application } from "express";
+import { Application, Response, Request } from "express";
 
 const app: Application = express();
 const port: number = 3000;
 
-app.get("/", (req: any, res: any) => res.send("Hello World!"));
+interface IRomanName {
+  praenomen: string;
+  nomen: string;
+  cognomen: string;
+}
+
+app.get("/", (_: Request, res: Response) => {
+  res.json({status: 'ok'})
+});
+
+app.get("/name", (_: Request, res: Response) => {
+  const name: IRomanName = {
+    praenomen: "Titus",
+    nomen: "Flavius",
+    cognomen: "Petro"
+  }
+  res.json({name: `${name.praenomen} ${name.nomen} ${name.cognomen}`});
+});
 
 app.listen(port, () => console.log(`API is listening on port ${port}!`));
